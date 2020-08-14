89123607ae8a0970e72044e2ecfaf6a8d7a3c8a0
Németh Tamás <tamas.nemeth2002@gmail.com>
2020-08-14 12:53:33 +0200

    Sound and graphics test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(Sounboard());
@@ -10,8 +15,71 @@ class Sounboard extends StatefulWidget {
 }
 
 class _SounboardState extends State<Sounboard> {
+  static AudioPlayer advPlayer = AudioPlayer();
+  static AudioCache player = AudioCache(fixedPlayer: advPlayer);
+
+  void logOnPress() {
+    print("object");
+  }
+
+  void playLocal(String filename) {
+    player.play(filename);
+  }
+
+  void stopSound() {
+    advPlayer.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Nasi Soundboard",
+      home: Scaffold(
+          appBar: new AppBar(
+            title: new Center(child: new Text("Nasi Soundboard")),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: new Column(
+              children: [
+                new Center(
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          RaisedButton(
+                              color: Colors.blue,
+                              textColor: Colors.white,
+                              padding: EdgeInsets.all(8.0),
+                              splashColor: Colors.blueAccent,
+                              onPressed: () => playLocal("zalan.wav"),
+                              child: Text(
+                                "Zalán",
+                                style: TextStyle(fontSize: 20.0),
+                              ))
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                new Expanded(
+                    child: new Align(
+                  alignment: Alignment.bottomCenter,
+                  child: new RaisedButton(
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    onPressed: () => stopSound(),
+                    child: new Text(
+                      "STOP!!!!",
+                      style: new TextStyle(fontSize: 30.0),
+                    ),
+                  ),
+                ))
+              ],
+            ),
+          )),
+    );
   }
 }
